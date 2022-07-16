3a63886c8bff7d057cba2329f9f1081b0cb20a77
whenes <whenes.oliveira@gmail.com>
2022-07-16 18:38:18 -0300

    Aplicação dos Builders e refatorações dos testes

---
+++ src/test/java/br/com/alura/leilao/dao/LeilaoDaoTest.java
@@ -0,0 +1,80 @@
+package br.com.alura.leilao.dao;
+
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
+import br.com.alura.leilao.util.builder.LeilaoBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeilaoDaoTest {
+    private LeilaoDao dao;
+    private EntityManager em;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JPAUtil.getEntityManager();
+        this.dao = new LeilaoDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
+
+    @Test
+    public void deveriaCadastrarUmLeilao() {
+        Usuario usuario = new UsuarioBuilder()
+                .comNome("Fulano")
+                .comEmail("fulano@fulano.com.br")
+                .comSenha("12345678")
+                .criar();
+        em.persist(usuario);
+
+        Leilao leilao = new LeilaoBuilder()
+                .comNome("Mochila")
+                .comValorInicial("500")
+                .comData(LocalDate.now())
+                .comUsuario(usuario)
+                .criar();
+        leilao = dao.salvar(leilao);
+        Leilao salvo = dao.buscarPorId(leilao.getId());
+        assertNotNull(salvo);
+    }
+
+    @Test
+    public void deveriaAtualizarUmLeilao() {
+        Usuario usuario = new UsuarioBuilder()
+                .comNome("Fulano")
+                .comEmail("fulano@fulano.com.br")
+                .comSenha("12345678")
+                .criar();
+        em.persist(usuario);
+
+        Leilao leilao = new LeilaoBuilder()
+                .comNome("Mochila")
+                .comValorInicial("500")
+                .comData(LocalDate.now())
+                .comUsuario(usuario)
+                .criar();
+        leilao = dao.salvar(leilao);
+
+        leilao.setNome("Celular");
+        leilao.setValorInicial(new BigDecimal("400"));
+        leilao = dao.salvar(leilao);
+
+        Leilao salvo = dao.buscarPorId(leilao.getId());
+        assertNotNull(salvo);
+        assertEquals("Celular", salvo.getNome());
+        assertEquals(new BigDecimal("400"), salvo.getValorInicial());
+    }
+}
--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -1,17 +1,64 @@
 package br.com.alura.leilao.dao;
 
 import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsuarioDaoTest {
     private UsuarioDao dao;
+    private EntityManager em;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JPAUtil.getEntityManager();
+        this.dao = new UsuarioDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
 
     @Test
-    public void testeBuscaDeUsuarioPeloUserName() {
-        this.dao = new UsuarioDao();
-        Usuario usuario = this.dao.buscarPorUsername("fulano");
-        assertNotNull(usuario);
+    public void deveriaEncontrarUsuarioCadastrado() {
+        Usuario usuario = new UsuarioBuilder()
+                .comNome("Fulano")
+                .comEmail("fulano@fulano.com.br")
+                .comSenha("12345678")
+                .criar();
+        em.persist(usuario);
+
+        Usuario usuarioEncontrado = this.dao.buscarPorUsername(usuario.getNome());
+        assertNotNull(usuarioEncontrado);
+    }
+    @Test
+    public void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+        Usuario usuario = new UsuarioBuilder()
+                .comNome("Fulano")
+                .comEmail("fulano@fulano.com.br")
+                .comSenha("12345678")
+                .criar();
+        em.persist(usuario);
+        assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("Beltrano"));
+    }
+
+    @Test
+    public void deveriaRemoverUmUsuario() {
+        Usuario usuario = new UsuarioBuilder()
+                .comNome("Fulano")
+                .comEmail("fulano@fulano.com.br")
+                .comSenha("12345678")
+                .criar();
+        em.persist(usuario);
+        dao.deletar(usuario);
+        assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("fulano"));
     }
 }
